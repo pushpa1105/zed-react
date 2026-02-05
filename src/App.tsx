f67d3058020fbe76c9387861bbdb75bbb99ef810
pushpa1105 <projectinfo1105@gmail.com>
2026-02-05 20:14:38 +0545

    add: not-found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import '@/App.css'
 import '@/index.css'
-import MainLayout from './layouts/MainLayout'
-import Home from '@/pages'
+import MainLayout from '@/layouts/MainLayout'
 import { ProtectedRoute } from '@/lib/auth/ProtectedRoute'
 import LoginPage from '@/pages/auth/login'
 import { GuestRoute } from '@/lib/auth/GuestRoute'
-import Demo from './pages/demo'
-import { AuthProvider } from './context/auth/AuthProvider'
+import Demo from '@/pages/demo'
+import { AuthProvider } from '@/context/auth/AuthProvider'
+import NotFound from '@/pages/not-found'
 
 function App() {
 
@@ -25,6 +25,8 @@ function App() {
                 <Route path='/login' element={<LoginPage />} />
               </Route>
             </Route>
+
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </AuthProvider>
       </Router>
